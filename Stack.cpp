9887b686a8e4b45d27c58d8d32f53572f65b4849
Dienes <dienes16@googlemail.com>
2014-05-18 19:40:35 +0200

    Added: Copyright for 2014

--- Stack.cpp
+++ Stack.cpp
@@ -1,23 +1,23 @@
 /*
- * RunFunge - A Befunge-93 compatible interpreter with extensions.
- * Copyright (C) 2009, 2011 Dienes
- *
- * This file is part of RunFunge.
- *
- * RunFunge is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Author E-Mail: dienes16 [at] googlemail [dot] com
+RunFunge - A Befunge-93 compatible interpreter with extensions.
+Copyright (C) 2009, 2011, 2014 Dienes
+
+This file is part of RunFunge.
+
+RunFunge is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Author E-Mail: dienes16 [at] googlemail [dot] com
 */
 
 #include "Stack.h"
